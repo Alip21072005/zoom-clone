7667eb45b2718bf0162f65b4011a86261869626c
Alip21072005 <muhamadalipmaulana3@gmail.com>
2025-01-04 22:46:34 +0700

    Refactor SignIn and SignUp pages to simplify layout and enhance component usage

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -1,13 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 
-export default function SignInPage() {
+export default function SiginInPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700">
-      <div className="bg-white p-8 rounded-lg shadow-xl w-full sm:w-[400px]">
-        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">Selamat Datang di SEE</h1>
-        <h2 className="text-lg text-center text-gray-600 mb-4">Masuk untuk melanjutkan</h2>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-      </div>
+    <main className="flex h-screen w-full items-center justify-center">
+      <SignIn />
     </main>
   );
 }
